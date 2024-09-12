f180dec7a017e91928ef3a22c01f147b87d453e0
amdymstfa <amdymoustapha011@gmail.com>
2024-09-12 17:17:16 +0100

    Challenge 1 : Table de Multiplication Inversée

---
+++ sas_day04/boucles/for_1.c
@@ -0,0 +1,10 @@
+// Challenge 1 : Table de Multiplication Inversée
+// Écrivez un programme C qui demande un nombre à l’utilisateur et affiche sa table de multiplication en ordre décroissant, de 10 à 1.
+//  Par exemple, pour le nombre 5, affichez :
+
+#include <stdio.h>
+
+int main(){
+    int n ;
+    printf("please enter a number : \n");
+}
