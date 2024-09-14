7c9a6112d90a3458aefe4747a38b2daa6921e5dd
amymstfa <amdymoustapha011@gmail.com>
2024-09-14 12:44:42 +0100

    Challenge 7 : Suite de Fibonacci

--- sas_day04/boucles/for_7.c
+++ sas_day04/boucles/for_7.c
@@ -1,23 +1,35 @@
 // Challenge 7 : Suite de Fibonacci
-// Écrivez un programme C qui génère les n premiers termes de la suite de Fibonacci, où n est entré par l’utilisateur. 
+// Écrivez un programme C qui génère les n premiers termes de la suite de Fibonacci, où n est entré par l’utilisateur.
 // La suite de Fibonacci est définie comme suit : F(0) = 0, F(1) = 1, et F(n) = F(n-1) + F(n-2).
 
 #include <stdio.h>
 
-int main(){
-    int n ;
-    printf("please enter a number !\n") ;
-    scanf("%d", &n) ;
-    if (n<0)
+int main()
+{
+    int n;
+    printf("please enter a number !\n");
+    scanf("%d", &n);
+    if (n < 0)
     {
-       printf("please enter a valide number!\n");
-    }else if (n == 0 ){
-        printf("F(%d) = %d\n", n, n) ;
-    }else if (n == 1){
-        printf("F(%d) = %d\n", n, n) ;
-    }else {
-        n = (n-1) + (n-2) ;
-        printf("F(%d) = %d\n", n-1, n) ;
+        printf("please enter a valide number!\n");
     }
-    
+    else if (n == 0)
+    {
+        printf("F(%d) = %d\n", n, n);
+    }
+    else if (n == 1)
+    {
+        printf("F(%d) = %d\n", n, n);
+    }
+    else
+    {
+        int a = 0, b = 1, fibonacci;
+        for (int i = 2; i <= n; i++)
+        {
+            fibonacci = a + b;
+            a = b;
+            b = fibonacci;
+        }
+        printf("F(%d) = %d\n", n, fibonacci);
+    }
 }
