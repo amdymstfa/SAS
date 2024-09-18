b03b27fe55a324c623b2eac94fd823e3a1f52cac
amymstfa <amdymoustapha011@gmail.com>
2024-09-18 21:04:36 +0100

    Challenge 4 : Structure avec Pointeurs

--- sas_day04_bis/structure/struct_4.c
+++ sas_day04_bis/structure/struct_4.c
@@ -7,29 +7,27 @@
 
 struct Point
 {
-   float *x ;
-   float *y ;
+    float x ;
+    float y ;
 };
 
-//inialize ponit
-void initialize(struct Point *p, float x_val,float y_val ){
-    p -> x = malloc(sizeof(float));
-    p -> y = malloc(sizeof(float));
-
-    if (p -> x == NULL || p -> y == NULL){
-        printf("allocation erreor\n");
-        exit(1);
-    }
-    *(p -> x) = x_val ;
-    *(p -> y) = y_val ;
-}
-
-void free(struct Point *p){
-    free(p -> x);
-    free(p -> y );
-}
-
-
 int main(){
-    
+    //declre variable of with pointers
+    struct Point *point ;
+    //initialization 
+    point = malloc(sizeof(struct Point));
+    // allocation error
+    if (!point){
+        fprintf(stderr, "allocation error");
+        return 1 ;
+    }
+    printf("please enter the value of x : ");
+    scanf("%f", &point->x) ;
+    printf("please enter the value of y : ");
+    scanf("%f", &point->y);
+
+    printf("(%.2f, %.2f)\n", point->x, point->y);
+
+    //liberation of memory 
+    free(point);
 }
